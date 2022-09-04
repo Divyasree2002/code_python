269b40d6d3ce212d025803ea1cfeaa3de90704e4
V.Divya Sree <76731589+Divyasree2002@users.noreply.github.com>
2022-09-04 22:16:07 +0530

    Add files via upload

---
+++ 03_VotingSystem.py
@@ -0,0 +1,6 @@
+num = int(input("enter the age : "))
+
+if num >= 18 :
+  print("You can vote")
+else :
+  print("You can not vote")
---
+++ 05_AllOperations.py
@@ -0,0 +1,17 @@
+n1 = int(input("Enter a number : "))
+n2 = int(input("enter a number : "))
+
+sum = n1 + n2
+sub = n1 - n2
+product = n1 * n2 
+quo = n1 / n2
+flr_quo = n1 // n2
+mod = n1 % n2
+power = pow(n1, n2)
+print("sum: ", sum)
+print("subtraction: ", sub)
+print("product: ", product)
+print("quotient: ", quo)
+print("floor quotient: ", flr_quo)
+print("modulus: ", mod)
+print("power: ", power)
